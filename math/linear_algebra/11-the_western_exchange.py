91a556971be1727ac5e0b697298164e103118e85
someonelse09 <burjuzada@gmail.com>
2025-08-09 11:30:25 +0000

    10-ill_use_my_scale

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This module includes the function
+called np_transpose """
+
+
+def np_transpose(matrix):
+    """ This function takes the transpose of the matrix """
+
+    return matrix.transpose
