204491bd7307b04cc5df70daffc4f1b123b0f7a6
someonelse09 <burjuzada@gmail.com>
2025-08-09 11:50:06 +0000

    13-cats_got_your_tongue

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -8,4 +8,4 @@ import numpy as np
 def np_cat(mat1, mat2, axis=0):
     """ This function concatenates the two matrices """
 
-    return concatenate((mat2, mat2), axis=axis)
+    return np.concatenate((mat1, mat2), axis=axis)
