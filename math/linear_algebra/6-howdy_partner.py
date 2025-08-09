29ef123fa7fdf79a525b73d109992338fbb57a38
someonelse09 <burjuzada@gmail.com>
2025-08-09 06:47:24 +0000

    6-howdy_partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module includes the function cat_arrays """
+
+
+def cat_arrays(arr1, arr2):
+    """ This function concatenates two arrays """
+
+    return arr1.extend(arr2)
