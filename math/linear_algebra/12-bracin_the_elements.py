4f7b6bc2a8657c5471ffb0817e82082ad2eef4a0
someonelse09 <burjuzada@gmail.com>
2025-08-09 11:40:58 +0000

    12-bracin_the_elements

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module includes the function 
+called np_elementwise """
+
+
+def np_elementwise(mat1, mat2):
+    """ This function allows users to take 
+    basic arithmetic operations(elementwise) on two matrices """
+
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
