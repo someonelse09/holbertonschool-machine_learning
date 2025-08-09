07d39553cb102111a1bd57449577516632cc2a1e
someonelse09 <burjuzada@gmail.com>
2025-08-09 11:56:22 +0000

    14-saddle_up

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,8 +2,10 @@
 """ This module contains function mat_mul 
 that is written using alternative method """
 
+import numpy as np
+
 
 def np_matmul(mat1, mat2):
     """ This function multiply two matrices """
 
-    return mat1 @ mat2
+    return np.matmul(mat1, mat2)
