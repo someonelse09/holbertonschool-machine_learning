00ce332869ef7beb4f0bdc3f750dbd2b77175169
someonelse09 <burjuzada@gmail.com>
2025-08-09 11:16:02 +0000

    10-ill_use_my_scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This module includes shape function """
+
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """ This function calculates the shape of the matrix """
+
+    return np.shape(matrix)
