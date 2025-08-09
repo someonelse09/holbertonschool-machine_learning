d1551d3060d49f7027237e45872c4e9e32fef12a
someonelse09 <burjuzada@gmail.com>
2025-08-09 11:24:56 +0000

    10-ill_use_my_scale

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -2,10 +2,7 @@
 """ This module includes shape function """
 
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
-
-
 def np_shape(matrix):
     """ This function calculates the shape of the matrix """
 
-    return tuple(matrix_shape(matrix))
+    return matrix.shape
