bc6ca663ff0a04ecd982dc71119de51b14c98205
someonelse09 <burjuzada@gmail.com>
2025-08-10 14:42:46 +0000

    100-slice_like_a_ninja

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" This module includes function to
+slice the matrix using numpy """
+
+
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """ This function slices the matrix
+    based on the given axis """
+
+    slices = []
+
+    for axis in range(matrix.ndim):
+        if axis in axes:
+            slice_tuple = axes[axis]
+            slices.append(slice(*slice_tuple))
+        else:
+            slice.append(slice(None))
+
+    return matrix[tuple(slices)]
