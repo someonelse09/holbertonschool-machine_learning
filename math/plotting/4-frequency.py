b5445f1daa54fbd935e4b758b1928757648395ac
someonelse09 <burjuzada@gmail.com>
2025-08-11 13:59:46 +0300

    4-frequency

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" This module includes 
+""" This module includes
 the function called frequency
 which decribes an histogram of scores """
 
@@ -9,14 +9,14 @@ import matplotlib.pyplot as plt
 
 
 def frequency():
-    """ This function plots 
+    """ This function plots
     histogram of student's scores """
 
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    
-    plt.hist(student_grades, bins = range(0, 101, 10), edgecolor = "black")
+
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
