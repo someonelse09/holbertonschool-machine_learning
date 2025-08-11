ce9c7ff02ffa78ff09cdede10b2676dbc6f4869e
someonelse09 <burjuzada@gmail.com>
2025-08-11 13:14:37 +0300

    0-line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+""" This Module includes the
+function named line """
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """ This function draws the
+    line graph determined by the code below """
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     x = np.arange(0, 11)
     plt.plot(x, y, 'r-')
+    plt.axis((0, 10, None, None))
     plt.show()
