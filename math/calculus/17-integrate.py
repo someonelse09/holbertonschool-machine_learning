5d7601be2c855577d4888d302fe8e7614848caf8
someonelse09 <burjuzada@gmail.com>
2025-08-13 10:10:13 +0000

    17-integrate

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" This module contain the function called poly_integral """
+
+
+def poly_derivative(poly, C=0):
+    """ This function calculates the
+    integral of the given polynomial """
+
+    if not isinstance(poly, list):
+        return None
+    if not all(isinstance(c, (int, float)) for c in poly):
+        return None
+    elif len(poly) == 0:
+        return [0]
+    result = [1/(x)*poly[x - 1] for x in range(1, len(poly) + 1)]
+    result.insert(0, 0)
+    return result
