e6c01a118a07ee58e3e2f5476b3d75a0548bb28b
someonelse09 <burjuzada@gmail.com>
2025-08-19 16:01:07 +0000

    1-build_decision_tree

---
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+""" This module includes class
+implementation of a decision tree """
+
+import numpy as np
+
+
+class Node:
+    """ class to interpret Nodes """
+    def __init__(self, feature=None, threshold=None, left_child=None,
+            right_child=None, is_root=False, depth=0):
+        """ Constructor of Node class """
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """ method to find maximum depth """
+        if self.left_child is None and self.right_child is None:
+            return self.depth
+        max_depth = self.depth
+        if self.left_child is not None:
+            max_depth = max(max_depth, self.left_child.max_depth_below())
+        if self.right_child is not None:
+            max_depth = max(max_depth, self.right_child.max_depth_below())
+        return max_depth
+
+    def count_nodes_below(self, only_leaves=False):
+        """ Method to count number of nodes"""
+        if only_leaves:
+            count = 0
+        else:
+            count = 1
+        if self.left_child is not None:
+            count += self.left_child.count_nodes_below(only_leaves)
+        if self.right_child is not None:
+            count += self.right_child.count_nodes_below()
+        return count
+class Leaf(Node):
+    """ class to interpret Leaves of the tree """
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """ method to return maximum depth """
+        return self.depth
+
+class Decision_Tree():
+    """ class to interpret Decision Trees """
+    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+        """ Constructor of Decision Tree class """
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self) :
+        """ method to return depth of the tree """
+        return self.root.max_depth_below()
