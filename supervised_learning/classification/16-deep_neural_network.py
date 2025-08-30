c6b57c570d2e9a0785c2396db1ca078dde1e27d2
someonelse09 <burjuzada@gmail.com>
2025-08-30 19:43:31 +0000

    Same Operations on Deep Neural Networks

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""This module includes the class DeepNeuralNetwork"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """This class is for implementing
+     multi-layered(more than two) Neural Networks"""
+    def __init__(self, nx, layers):
+        """Initalising the Deep Neural Network"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(x, int) and x > 0 for x in layers):
+            raise ValueError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for lx in range(1, self.L + 1):
+            if lx == 1:
+                previous_nodes = nx
+            else:
+                previous_nodes = layers[lx - 2]
+            self.weights['W' + str(lx)] = (
+                np.random.randn(layers[lx - 1], previous_nodes)
+                * np.sqrt(2/previous_nodes)
+            )
+            self.weights['b' + str(lx)] = np.zeros((layers[lx - 1], 1))
---
+++ supervised_learning/classification/19-deep_neural_network.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""This module includes the class DeepNeuralNetwork"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """This class is for implementing
+     multi-layered(more than two) Neural Networks"""
+
+    def __init__(self, nx, layers):
+        """Initalising the Deep Neural Network"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(x, int) and x > 0 for x in layers):
+            raise ValueError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for lx in range(1, self.L + 1):
+            if lx == 1:
+                previous_nodes = nx
+            else:
+                previous_nodes = layers[lx - 2]
+            self.weights['W' + str(lx)] = (
+                    np.random.randn(layers[lx - 1], previous_nodes)
+                    * np.sqrt(2 / previous_nodes)
+            )
+            self.weights['b' + str(lx)] = np.zeros((layers[lx - 1], 1))
+
+    @property
+    def L(self):
+        """getter for number of layers in the DNN"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """getter method for
+        intermediary values of the DNN"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """getter method for weights
+        and biases of the DNN"""
+        return self.__weights
+
+    def forward_prop(self, X):
+        """This method calculates the forward
+         propagation of the neural network"""
+        self.__cache['A0'] = X
+        for lx in range(1, self.__L + 1):
+            W = self.__weights['W' + str(lx)]
+            b = self.__weights['b' + str(lx)]
+            previous_A = self.__cache['A' + str(lx - 1)]
+
+            Z = np.matmul(W, previous_A) + b
+
+            if lx == self.__L:
+                A = 1 / (1 + np.exp(-Z))
+            else:
+                A = np.maximum(0, Z)
+            self.__cache['A' + str(lx)] = A
+        return self.__cache['A' + str(self.__L)], self.__cache
+
+    def cost(self, Y, A):
+        """Calculates the cost of
+        the model using logistic regression"""
+        m = Y.shape[1]
+        cost = -((1/m)*(np.sum((1 - Y) * np.log(1.0000001 - A) + Y * np.log(A))))
+        return cost
