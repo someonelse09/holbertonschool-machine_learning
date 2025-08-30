9039f762d4645826e5334c211fd33dbfc24eb0a2
someonelse09 <burjuzada@gmail.com>
2025-08-30 20:19:52 +0000

    17-deep_neural_network

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -7,28 +7,33 @@ import numpy as np
 class DeepNeuralNetwork:
     """This class is for implementing
      multi-layered(more than two) Neural Networks"""
+
     def __init__(self, nx, layers):
-        """Initalising the Deep Neural Network"""
+        """Initialising the Deep Neural Network"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(x, int) and x > 0 for x in layers):
-            raise ValueError("layers must be a list of positive integers")
+
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
-        for lx in range(1, self.L + 1):
+        for lx in range(1, self.__L + 1):
+            # Validate each layer element during the loop
+            if not isinstance(layers[lx - 1], int) or layers[lx - 1] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+
             if lx == 1:
                 previous_nodes = nx
             else:
                 previous_nodes = layers[lx - 2]
+
             self.weights['W' + str(lx)] = (
-                np.random.randn(layers[lx - 1], previous_nodes)
-                * np.sqrt(2/previous_nodes)
+                np.random.randn(layers[lx - 1], previous_nodes) *
+                np.sqrt(2 / previous_nodes)
             )
             self.weights['b' + str(lx)] = np.zeros((layers[lx - 1], 1))
 
