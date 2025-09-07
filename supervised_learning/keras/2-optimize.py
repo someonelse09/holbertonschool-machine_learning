b007161ed6d5f07c01b1967eaaef191aee15d9bc
someonelse09 <burjuzada@gmail.com>
2025-09-07 12:12:03 +0000

    Optimising the model

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Optimizes the neural network using the Keras library"""
 
-import tensorflow as tf
+import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
     """This function sets up Adam optimization
     for a keras model with categorical crossentropy
     loss and accuracy metrics"""
-    optimizer = tf.keras.optimizers.Adam(
+    optimizer = K.optimizers.Adam(
         learning_rate=alpha,
         beta_1=beta1
         beta_2=beta2
