b186348e1611d8a1358135bb02a75c320ed30041
someonelse09 <burjuzada@gmail.com>
2025-09-07 12:05:28 +0000

    with Input and Model classes

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -12,7 +12,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         kernel_regularizer=K.regularizers.l2(lambtha)
     )(inputs)
 
-    if keep_prob < 1 and  len(layers) > 1:
+    if keep_prob < 1 and len(layers) > 1:
         x = K.layers.Dropout(1 - keep_prob)(x)
 
     for i in range(1, len(layers)):
