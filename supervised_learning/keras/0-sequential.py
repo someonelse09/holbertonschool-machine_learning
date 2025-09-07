2ca02d603e4e13e81037a0dbc6aaa533a1c004b1
someonelse09 <burjuzada@gmail.com>
2025-09-07 08:19:12 +0000

    Sequential model

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python3
-"""Builds a neural network with the Keras library"""
+"""This module includes the function
+which generates models using tensorflow keras"""
+
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """Builds a neural network with the Keras Functional API"""
-    inputs = K.Input(shape=(nx,))
-    x = K.layers.Dense(
-        units=layers[0],
-        activation=activations[0],
-        kernel_regularizer=K.regularizers.l2(lambtha)
-    )(inputs)
-
+    """Building a Neural Network with
+    Sequential model fromKeras API"""
+    model = K.Sequential()
+    model.add(K.layers.Dense(units=layers[0],
+                    activation=activations[0],
+                    kernel_regularizer=K.regularizers.l2(lambtha),
+                    input_shape=(nx,)))
     if keep_prob < 1:
-        x = K.layers.Dropout(1 - keep_prob)(x)
-
+        model.add(K.layers.Dropout(1 - keep_prob))
     for i in range(1, len(layers)):
-        x = K.layers.Dense(
-            units=layers[i],
-            activation=activations[i],
-            kernel_regularizer=K.regularizers.l2(lambtha)
-        )(x)
-
+        model.add(K.layers.Dense(units=layers[i],
+                  activation=activations[i],
+                  kernel_regularizer=K.regularizers.l2(lambtha)))
         if i < len(layers) - 1 and keep_prob < 1:
-            x = K.layers.Dropout(1 - keep_prob)(x)
-
-    model = K.Model(inputs=inputs, outputs=x)
+            model.add(K.layers.Dropout(1 - keep_prob))
     return model
