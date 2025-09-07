671b9aa8c243f89a54805ecc5a1d7f667e23142c
someonelse09 <burjuzada@gmail.com>
2025-09-07 12:03:31 +0000

    with Input and Model classes

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-"""This module includes the function
-for building a model with Tensorflow"""
-
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras import Model, Input
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.regularizers import l2
-import numpy as np
+"""Builds a neural network with the Keras library"""
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """This function builds a neural network
-    using Input and Model classes of Keras"""
-    inputs = Input(shape=(nx,))
-    m = Dense(units=layers[0],
-                    activations=activations[0],
-                    kernel_regularizer=l2(lambtha))(inputs)
-    if keep_prob < 1:
-        m = Dropout(1 - keep_prob)(m)
+    """Builds a neural network with the Keras Functional API"""
+    inputs = K.Input(shape=(nx,))
+    x = K.layers.Dense(
+        units=layers[0],
+        activation=activations[0],
+        kernel_regularizer=K.regularizers.l2(lambtha)
+    )(inputs)
+
+    if keep_prob < 1 and  len(layers) > 1:
+        x = K.layers.Dropout(1 - keep_prob)(x)
+
     for i in range(1, len(layers)):
-        m = (units=layers[i],
-                  activations=activations[i],
-                  kernel_regularizer=l2(lambtha))(m)
-    if i < len(layers) and keep_prob < 1:
-        m = (Dropout(1 - keep_prob))(m)
-    model = Model(inputs=inputs, outputs=m)
+        x = K.layers.Dense(
+            units=layers[i],
+            activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)
+        )(x)
+
+        if i < len(layers) - 1 and keep_prob < 1:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+
+    model = K.Model(inputs=inputs, outputs=x)
     return model
