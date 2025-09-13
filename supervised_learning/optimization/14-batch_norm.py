cccc5e6f408a09f6dc4d14c228701b9f930ca015
someonelse09 <burjuzada@gmail.com>
2025-09-13 16:05:40 +0400

    Update 14-batch_norm.py

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""This module includes the function that creates a batch normalization layer for a neural network in tensorflow"""
+"""This module includes the function that
+creates a batch normalization layer for
+a neural network in tensorflow"""
 
 import tensorflow as tf
 
@@ -10,8 +12,11 @@ def create_batch_norm_layer(prev, n, activation):
         prev is the activated output of the previous layer
         n is the number of nodes in the layer to be created
         activation is the activation function that should be used on the output of the layer
-        you should use the tf.keras.layers.Dense layer as the base layer with kernel initializer tf.keras.initializers.VarianceScaling(mode='fan_avg')
-        your layer should incorporate two trainable parameters, gamma and beta, initialized as vectors of 1 and 0 respectively
+        you should use the tf.keras.layers.Dense layer
+            as the base layer with kernel initializer
+                tf.keras.initializers.VarianceScaling(mode='fan_avg')
+        your layer should incorporate two trainable parameters,
+            gamma and beta, initialized as vectors of 1 and 0 respectively
         you should use an epsilon of 1e-7
     Returns:
         a tensor of the activated output for the layer
@@ -19,7 +24,8 @@ def create_batch_norm_layer(prev, n, activation):
     # Create dense layer without activation
     dense = tf.keras.layers.Dense(
         n,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(mode='fan_avg'),
+        kernel_initializer=tf.keras.initializers.
+                           VarianceScaling(mode='fan_avg'),
         use_bias=False  # We'll handle bias through beta parameter
     )(prev)
 
