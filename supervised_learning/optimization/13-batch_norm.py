8da9d09824351ca83789bfc02ea4e3e602655ddf
someonelse09 <burjuzada@gmail.com>
2025-09-13 16:08:34 +0400

    Update 13-batch_norm.py

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -12,8 +12,10 @@ def batch_norm(Z, gamma, beta, epsilon):
         Z is a numpy.ndarray of shape (m, n) that should be normalized
         m is the number of data points
         n is the number of features in Z
-        gamma is a numpy.ndarray of shape (1, n) containing the scales used for batch normalization
-        beta is a numpy.ndarray of shape (1, n) containing the offsets used for batch normalization
+        gamma is a numpy.ndarray of shape (1, n)
+            containing the scales used for batch normalization
+        beta is a numpy.ndarray of shape (1, n)
+            containing the offsets used for batch normalization
         epsilon is a small number used to avoid division by zero
     Returns:
         the normalized Z matrix
