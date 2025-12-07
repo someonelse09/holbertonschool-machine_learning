43c52394a71e5772f90d6cf857e2d611e630fb83
Gurban_Burjuzada <burjuzada@gmail.com>
2025-12-07 23:49:15 +0400

    Update 10-kmeans.py

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,31 +1,30 @@
-#!/usr/bin/env/python3
-"""This module includes the function kmeans
-that performs K-means on a dataset"""
-
+#!/usr/bin/env python3
+"""Perform K-means with sklearn."""
 import sklearn.cluster
 
 
 def kmeans(X, k):
     """
+    Performs K-means clustering on a dataset.
+
     Args:
-        X is a numpy.ndarray of shape (n, d) containing the dataset
-        k is the number of clusters
-        The only import you are allowed to use is import sklearn.cluster
+        X (numpy.ndarray): Dataset of shape (n, d)
+        k (int): Number of clusters
+
     Returns:
-        C, clss
-        C is a numpy.ndarray of shape (k, d)
-         containing the centroid means for each cluster
-        clss is a numpy.ndarray of shape (n,) containing the
-         index of the cluster in C that each data point belongs to
+        tuple: (C, clss)
+            - C is a numpy.ndarray of shape (k, d) containing the centroids
+            - clss is a numpy.ndarray of shape (n,) with cluster assignments
     """
-    # Create KMeans object and fit to data
-    kmeans_model = sklearn.cluster.KMeans(n_clusters=k)
-    kmeans_model.fit(X)
+    if not hasattr(X, "shape") or X.ndim != 2:
+        return None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None
 
-    # Get cluster centers (centroids)
-    C = kmeans_model.cluster_centers_
+    model = sklearn.cluster.KMeans(n_clusters=k)
+    model.fit(X)
 
-    # Get cluster assignments for each point
-    clss = kmeans_model.labels_
+    C = model.cluster_centers_
+    clss = model.labels_
 
     return C, clss
