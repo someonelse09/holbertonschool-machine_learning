e673b063b1c96cc25c2e48dc26c5dc463c61e702
Gurban_Burjuzada <burjuzada@gmail.com>
2025-12-19 16:49:22 +0400

    Update 1-sparse.py

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -33,11 +33,11 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     for nodes in hidden_layers:
         encoded = keras.layers.Dense(nodes, activation='relu')(encoded)
 
+    ar = keras.regularizers.l1(lambtha)
     # Latent Space (Bottleneck)
     latent = keras.layers.Dense(latent_dims,
                                 activation='relu',
-                                activity_regularizer=
-                                keras.regularizers.l1(lambtha))(encoded)
+                                activity_regularizer=ar)(encoded)
 
     encoder = keras.Model(inputs, latent)
 
