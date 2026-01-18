a4f0afa9da2c5b0a21fae8786d603ff2c532ac39
Gurban_Burjuzada <burjuzada@gmail.com>
2026-01-18 22:39:48 +0400

    Update 1-rnn.py

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -14,13 +14,11 @@ class RNNCell:
             o is the dimensionality of the outputs
             Creates the public instance attributes Wh, Wy, bh,
              by that represent the weights and biases of the cell
-
             Wh and bh are for the concatenated hidden state and input data
             Wy and by are for the output
-            The weights should be initialized using a random normal distribution in the order listed above
-
+            The weights should be initialized using a random
+            normal distribution in the order listed above
             The weights will be used on the right side for matrix multiplication
-
             The biases should be initialized as zeros"""
         # Wh: weight matrix for concatenated [h_prev, x_t] -> h_next
         # Shape: (i + h, h) because we concat input(i)
@@ -36,9 +34,11 @@ class RNNCell:
     def forward(self, h_prev, x_t):
         """
         Args:
-            x_t is a numpy.ndarray of shape (m, i) that contains the data input for the cell
+            x_t is a numpy.ndarray of shape (m, i)
+            that contains the data input for the cell
             m is the batche size for the data
-            h_prev is a numpy.ndarray of shape (m, h) containing the previous hidden state
+            h_prev is a numpy.ndarray of shape (m, h)
+            containing the previous hidden state
             The output of the cell should use a softmax activation function
         Returns:
              h_next, y
@@ -46,7 +46,8 @@ class RNNCell:
             y is the output of the cell
         """
         # Concatenate h_prev and x_t along axis 1 (features)
-        # h_prev shape: (m, h), x_t shape: (m, i) -> concat shape: (m, h+i)
+        # h_prev shape: (m, h), x_t shape:
+        # (m, i) -> concat shape: (m, h+i)
         concat = np.concatenate((h_prev, x_t), axis=1)
 
         # Compute next hidden state with tanh activation
@@ -67,12 +68,15 @@ class RNNCell:
 def rnn(rnn_cell, X, h_0):
     """
     Args:
-        rnn_cell is an instance of RNNCell that will be used for the forward propagation
-        X is the data to be used, given as a numpy.ndarray of shape (t, m, i)
+        rnn_cell is an instance of RNNCell that
+        will be used for the forward propagation
+        X is the data to be used, given as a
+        numpy.ndarray of shape (t, m, i)
         t is the maximum number of time steps
         m is the batch size
         i is the dimensionality of the data
-        h_0 is the initial hidden state, given as a numpy.ndarray of shape (m, h)
+        h_0 is the initial hidden state, given as a
+        numpy.ndarray of shape (m, h)
         h is the dimensionality of the hidden state
     Returns:
         H, Y
